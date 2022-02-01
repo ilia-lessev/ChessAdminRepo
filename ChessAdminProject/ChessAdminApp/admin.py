b9c24ae67d6be5493cec22bd6d1591edfa6789c6
Ilia Lessev <ilia.lessev@gmail.com>
2022-02-01 21:03:36 +0200

    Add base Player and Match models

--- ChessAdminProject/ChessAdminApp/admin.py
+++ ChessAdminProject/ChessAdminApp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Player
+from .models import Match
 from django.forms import ModelForm
 from django.db.models import F, Q, ExpressionWrapper, IntegerField
 from django.db.models import Value, Count, OuterRef, Subquery, Sum
@@ -6,7 +8,8 @@ from django.db.models.expressions import RawSQL
 
 admin.site.site_header = 'Netstock Chess Club - Admin Site'
 
-
+admin.site.register(Player)
+admin.site.register(Match) 
 
 
 
--- ChessAdminProject/ChessAdminApp/models.py
+++ ChessAdminProject/ChessAdminApp/models.py
@@ -8,5 +8,147 @@ from computed_property import ComputedIntegerField
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
+MATCH_RESULT_CHOICES = [("WIN", "WIN"), ("DRAW", "DRAW"),]
+MATCH_STATUS_CHOICES = [("SCHEDULED", "SCHEDULED"),("PLAYING", "PLAYING"),("COMPLETED", "COMPLETED"),("CANCELED", "CANCELED")]
+BOOLEAN_CHOICES = [(True,'Yes'),(False,'No')]
 
-  
+        
+class Player(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(null=True, blank=True)
+    date_of_birth = models.DateTimeField(null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    is_active = models.BooleanField(max_length=100, null=True, blank=True, choices=BOOLEAN_CHOICES)
+    ranking = models.IntegerField(null=True, blank=True, default=0)
+    points = models.IntegerField(null=True, blank=True, default=0)
+                                
+    def __str__(self):
+        return f"Player: {self.first_name} {self.last_name}  | Current Ranking: {self.ranking}  "
+        
+    def save(self, *args, **kwargs):
+        _all_players_count = Player.objects.count()
+        if self._state.adding:
+            self.ranking = _all_players_count + 1
+            
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+        
+    class Meta:                                                                                                          
+        verbose_name_plural = " Players"
+    
+class Match(models.Model):
+    scheduled_date = models.DateTimeField(null=True, blank=True, default=datetime.now)
+    status = models.CharField(max_length=50, null=True, blank=False, choices=MATCH_STATUS_CHOICES, default='SCHEDULED')
+    player_one = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_one', null=True, blank=True)
+    player_two = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='player_two', null=True, blank=True)
+    venue = models.CharField(max_length=100, null=True, blank=True)
+    match_result = models.CharField(max_length=100, null=True, blank=True, choices=MATCH_RESULT_CHOICES)
+    winner = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='winner', null=True, blank=True)
+    player_one_entry_ranking = models.IntegerField(null=True, blank=True)
+    player_two_entry_ranking = models.IntegerField(null=True, blank=True)
+    player_one_ranking_change = models.IntegerField(null=True, blank=True)
+    player_two_ranking_change = models.IntegerField(null=True, blank=True)
+    player_one_points_change = models.IntegerField(null=True, blank=True)
+    player_two_points_change = models.IntegerField(null=True, blank=True)
+    follow_live = models.CharField(max_length=255, null=True, blank=True, default='https://<LINK TO BE COMPLETED>')
+      
+    def __str__(self):
+        return f"Current Status: {self.status}  |  Date: {self.scheduled_date}  |  {self.player_one.first_name} {self.player_one.last_name} vs {self.player_two.first_name} {self.player_two.last_name}" if self.player_one and self.player_two else f"Current Status: {self.status}  |  Date: {self.scheduled_date}"
+        
+    def save(self, *args, **kwargs):
+        
+        if self.status == "PLAYING":
+            self.player_one_entry_ranking = self.player_one.ranking
+            self.player_two_entry_ranking = self.player_two.ranking
+        
+        if self.status == "COMPLETED":
+        
+            _player_one_entry_ranking = self.player_one.ranking
+            _player_two_entry_ranking = self.player_two.ranking
+            self.player_one_entry_ranking = self.player_one.ranking
+            self.player_two_entry_ranking = self.player_two.ranking
+            
+            _higher_rank_value=_player_one_entry_ranking if _player_one_entry_ranking < _player_two_entry_ranking else _player_two_entry_ranking    
+            _lower_rank_value=_player_one_entry_ranking if _player_one_entry_ranking > _player_two_entry_ranking else _player_two_entry_ranking
+            
+            if self.match_result == "DRAW":
+                _higher_rank_shift=0
+                _lower_rank_shift = 0 if (_lower_rank_value - _higher_rank_value) == 1 else -1
+                if _player_one_entry_ranking < _player_two_entry_ranking:
+                    self.player_one_ranking_change = _higher_rank_shift
+                    self.player_two_ranking_change = _lower_rank_shift
+                else:
+                    self.player_one_ranking_change = _lower_rank_shift
+                    self.player_two_ranking_change = _higher_rank_shift
+                _player_to_shift = Player.objects.get(ranking=_lower_rank_value-1)      
+                _player_to_shift.ranking += 1
+                _player_to_shift.save()
+                self.player_one.ranking = _player_one_entry_ranking + self.player_one_ranking_change 
+                self.player_one.save()   
+                self.player_two.ranking = _player_two_entry_ranking + self.player_two_ranking_change
+                self.player_two.save()
+            
+            if self.match_result == "WIN":
+                _rank_difference = abs(_player_one_entry_ranking - _player_two_entry_ranking)
+                _winner_points = 0 
+                _looser_points = 0 
+                 
+                if ((_player_one_entry_ranking < _player_two_entry_ranking and self.winner == self.player_one) or (_player_two_entry_ranking < _player_one_entry_ranking and self.winner == self.player_two)):
+                    self.player_one_ranking_change = 0
+                    self.player_two_ranking_change = 0
+                    _winner_points = 5 if (_rank_difference>=15) else 8 if (_rank_difference>=10) else 10
+                    _looser_points = -5 if (_rank_difference>=15) else -8 if (_rank_difference>=10) else -10
+                
+                    if (self.winner == self.player_one):
+                        self.player_one_points_change = _winner_points
+                        self.player_two_points_change = _looser_points
+                    else:
+                        self.player_one_points_change = _looser_points
+                        self.player_two_points_change = _winner_points 
+                    
+                else:
+                    _winner_points = 50 if (_rank_difference>=15) else 25 if (_rank_difference>=10) else 15
+                    _looser_points = -50 if (_rank_difference>=15) else -25 if (_rank_difference>=10) else -15
+                    
+                    _higher_rank_shift=0 if ((_lower_rank_value - _higher_rank_value)==2) else 1
+                    _lower_rank_shift=-1 if ((_lower_rank_value - _higher_rank_value)==1) else math.floor((_lower_rank_value - _higher_rank_value) / 2) * -1
+                    _player_to_shift = Player.objects.get(ranking=_higher_rank_value+1)       
+                    _player_to_shift.ranking -= _higher_rank_shift
+                    _player_to_shift.save()
+                    
+                    self.player_one_points_change = 0
+                    self.player_two_points_change = 0
+                    self.player_one_ranking_change = 0
+                    self.player_two_ranking_change = 0
+                    
+                    if (self.winner == self.player_one):
+                        self.player_one_ranking_change = _lower_rank_shift 
+                        self.player_two_ranking_change = _higher_rank_shift # if ((_lower_rank_value - _higher_rank_value)!=2) else 0
+                        self.player_one_points_change = _winner_points
+                        self.player_two_points_change = _looser_points
+                        Player.objects.filter(ranking__gte=(_player_one_entry_ranking + self.player_one_ranking_change),ranking__lte=_lower_rank_value).update(ranking=F('ranking')+1)    #,ranking__lte=_lower_rank_value,ranking!=_lower_rank_value
+                    if (self.winner == self.player_two):
+                        self.player_one_ranking_change = _higher_rank_shift #if ((_lower_rank_value - _higher_rank_value)!=2) else 0
+                        self.player_two_ranking_change = _lower_rank_shift 
+                        self.player_one_points_change = _looser_points
+                        self.player_two_points_change = _winner_points
+                        Player.objects.filter(ranking__gte=(_player_two_entry_ranking + self.player_two_ranking_change), ranking__lte=_lower_rank_value).update(ranking=F('ranking')+1)
+                    
+                _cur_player_one_points = self.player_one.points
+                _cur_player_two_points = self.player_two.points
+                if ((_cur_player_one_points + self.player_one_points_change) < 0):
+                    self.player_one_points_change = _cur_player_one_points * -1    
+                if ((_cur_player_two_points + self.player_two_points_change) < 0):
+                    self.player_two_points_change = _cur_player_two_points * -1 
+                
+                self.player_one.ranking = _player_one_entry_ranking + self.player_one_ranking_change 
+                self.player_one.points += self.player_one_points_change 
+                self.player_one.save()   
+                
+                self.player_two.ranking = _player_two_entry_ranking + self.player_two_ranking_change
+                self.player_two.points += self.player_two_points_change 
+                self.player_two.save()
+        
+        super().save(*args, **kwargs)  
+        
